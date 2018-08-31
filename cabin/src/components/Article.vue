a215b46baef6c1db7f4baa030649e37f30a507f6
sryanyuan <sryan@qq.com>
2018-08-31 13:25:24 +0800

    article breadcrumb

--- cabin/src/components/Article.vue
+++ cabin/src/components/Article.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div id="breadcrumb-container">
+            <div id="breadcrumbs">
+                <span class="islink" @click="toCategory">{{article.category}}</span>
+                <i class="el-icon-arrow-right seperator"></i>
+                <span>{{article.title}}</span>
+            </div>
+        </div>
         <article-container :summary="summary" :key="articleId" :articleId="articleId"></article-container>
         <comments :toWho="toWho" :uri="commentURI"></comments>
     </div>
@@ -24,6 +31,11 @@ export default {
             return "article:" + this.articleId
         }
     },
+    methods: {
+        toCategory() {
+            this.$router.push({name: 'blogCategory', params: {id: this.article.categoryId}, query: {page: 0}})
+        }
+    },
     components: {
         ArticleContainer,
         Comments
@@ -40,9 +52,37 @@ export default {
                 self.$message.warning(res.error)
             }
         }, this.articleId, 1)
-    }
+    },
+    props: []
 }
 </script>
 
 <style scoped>
+#breadcrumb-container {
+    margin-bottom: 10px;
+}
+
+#breadcrumbs span.islink {
+    font-weight: 700;
+    cursor: pointer;
+}
+
+#breadcrumbs span {
+    font-size: 14px;
+    line-height: 1;
+}
+
+#breadcrumbs span.islink:hover {
+  color: #2ebaae;
+  transition: all 0.5s;
+}
+
+#breadcrumbs .seperator {
+    margin: 0 6px;
+    font-weight: 400;
+    color: #c0c4cc;
+    display: inline-block;
+    vertical-align: baseline;
+    font-size: 14px;
+}
 </style>
